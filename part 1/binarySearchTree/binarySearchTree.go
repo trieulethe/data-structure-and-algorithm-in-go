92b8df7c755ebde6da1bc1973162be91c4aed24c
trieu <amhanhnguxu@gmail.com>
2019-01-10 22:14:12 +0700

    fixed done

--- part 1/binarySearchTree/binarySearchTree.go
+++ part 1/binarySearchTree/binarySearchTree.go
@@ -15,7 +15,6 @@ type Node struct {
 	parent *Node
 	left   *Node
 	right  *Node
-	// addrPointer int
 }
 
 func (*Node) insertNode(n *Node, index int, v int) bool {
@@ -65,41 +64,24 @@ func (*Tree) searchValue(t *Tree, k int) bool {
 	return t.root.binarySearchNode(t.root, k)
 }
 
-func (*Node) searchIndex(n *Node, id int) **Node {
-	// fmt.Println()
-	fmt.Println("node >> search: ", n)
-	// fmt.Println(reflect.TypeOf(n))
-	// fmt.Println(reflect.TypeOf(&n))
-	fmt.Println("node >> search: ", &n)
-	// fmt.Println()
-
+func searchIndex(n *Node, id int) *Node {
 	if n == nil {
 		return nil
 	}
-	if id == n.index {
-		fmt.Println("n index: ", n)
-		fmt.Println("n index: ", &n)
-		return &n
-	}
-	// else if  {
-	// 	return n.searchIndex(n.left, id)
-	// } else {
-	// 	return n.searchIndex(n.right, id)
-	// }
 
-	node := n.searchIndex(n.left, id)
-	if node == nil {
-		node = n.searchIndex(n.right, id)
+	if id == n.index {
+		return n
 	}
-	fmt.Println("node finded return: ", node)
-	*node = nil
-	// fmt.Println("node finded return: ", &node)
-	// fmt.Println("node finded return: ", *node)
-	// println(">> : ", &node)
+
+	node := searchIndex(n.left, id)
+	if node == nil {
+		node = searchIndex(n.right, id)
+	}
+
 	return node
 }
 
-func (*Node) checkTypeNode(n *Node) string {
+func checkTypeNode(n *Node) string {
 	if n.left == nil && n.right == nil {
 		return "leaf"
 	} else if (n.left == nil && n.right != nil) || (n.left != nil && n.right == nil) {
@@ -109,34 +91,53 @@ func (*Node) checkTypeNode(n *Node) string {
 	}
 }
 
-func (*Node) removeLeaf(n *Node) *Node {
-	n = nil
-	return n
+func checkParent(n *Node) string {
+	if n.parent.left == n {
+		return "left"
+	}
+	return "right"
 }
 
-func (*Node) removeChild(n *Node) *Node {
+func removeLeaf(n *Node) {
+	parent := n.parent
+	if parent.left == n {
+		parent.left = nil
+		n = &Node{}
+		return
+	}
+	parent.right = nil
+	n = &Node{}
+	return
+}
+
+func removeChild(n *Node) {
+	fmt.Println("node n:", n)
+	fmt.Println("node *n:", *n)
+	temp := *n
 	if n.left == nil {
-		// parent := n.parent
-		n = n.right
-		n.right = nil
-		fmt.Println("delete child n right: ", n)
-	} else {
-		n = n.left
-		n.left = nil
+		*n = *n.right
+		n.parent = temp.parent
+		return
 	}
 
-	return n
+	*n = *n.left
+	n.parent = temp.parent
+	return
 }
 
-func (*Node) removeInternal(n *Node) *Node {
-	temp := n.getElementSideLeft(n)
-	fmt.Println("node temp: ", temp)
-	// temp.removeChild(n)
-	return temp.removeChild(n)
-
-	// n = temp
-	// fmt.Println("node copy temp: ", n)
-	// return n
+func removeInternal(n *Node) {
+	fmt.Println("node internal:", n)
+	parent := n.parent
+	fmt.Println("parent node:", parent)
+	fmt.Println("parent node:", parent.right)
+	fmt.Println("parent node:", *parent.right)
+	nChildLeft := n.getElementSideLeft(n.right)
+	fmt.Println("left node:", nChildLeft)
+	n.value = nChildLeft.value
+	n.index = nChildLeft.index
+	fmt.Println("node changed:", n)
+	removeLeaf(nChildLeft)
+	return
 }
 
 func (*Node) getElementSideLeft(n *Node) *Node {
@@ -146,40 +147,21 @@ func (*Node) getElementSideLeft(n *Node) *Node {
 	return n.getElementSideLeft(n.left)
 }
 
-func (*Tree) remove(t *Tree, id int) *Node {
+func (*Tree) remove(t *Tree, id int) {
 	if id == 0 {
 		fmt.Println("cureent can not remove root")
 	}
-	fmt.Println("root: ", t.root)
-	fmt.Println("address root: ", *t.root)
-	fmt.Println("address root: ", &t.root)
-	node := t.root.searchIndex(t.root, id)
-	fmt.Println("finded node: ", node)
-	fmt.Println("finded node: ", *node)
-	// println(node)
-	// p := node
-	// // *p = nil
-	// fmt.Println("p: ", p)
-	// fmt.Println(reflect.TypeOf(p))
-	// fmt.Println(p)
-	// fmt.Println(reflect.TypeOf(*p))
-	// fmt.Println(*p)
-	// fmt.Println(reflect.TypeOf(**p))
-	// fmt.Println(**p)
-	// *p = nil
+	node := searchIndex(t.root, id)
+	checkT := checkTypeNode(node)
+	if checkT == "leaf" {
+		removeLeaf(node)
+	} else if checkT == "child" {
+		removeChild(node)
+	} else {
+		removeInternal(node)
+	}
 
-	// checkType := node.checkTypeNode(node)
-	// fmt.Println("check type node: ", checkType)
-
-	// if checkType == "leaf" {
-	// 	node = node.removeLeaf(node)
-	// } else if checkType == "child" {
-	// 	node = node.removeChild(node)
-	// } else {
-	// 	node = node.removeInternal(node)
-	// 	fmt.Println("node removed internal: ", node)
-	// }
-	return nil
+	return
 }
 
 func (*Node) printNode(n *Node) {
@@ -201,119 +183,23 @@ func (*Tree) printTree(t *Tree) {
 }
 
 func main() {
-	t := Tree{}
-	fmt.Println("tree > : ", t)
+	t := &Tree{}
 
-	i := t.insertTree(&t, 34)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 17)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 25)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 66)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 50)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 71)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 68)
-	fmt.Println("tree > : ", t, i)
-
-	i = t.insertTree(&t, 75)
-	fmt.Println("tree > : ", t, i)
+	t.insertTree(t, 34)
+	t.insertTree(t, 17)
+	t.insertTree(t, 25)
+	t.insertTree(t, 66)
+	t.insertTree(t, 50)
+	t.insertTree(t, 71)
+	t.insertTree(t, 68)
+	t.insertTree(t, 75)
+	t.printTree(t)
 
 	fmt.Println()
 
-	var n = Node{}
-	fmt.Println("node: ", n)
-	fmt.Println("&node: ", &n)
-	p := &n
-	fmt.Println("p: ", p)
-	fmt.Println("*&node: ", *&n)
-	fmt.Println("*&node: ", *&n)
+	t.remove(t, 3)
 
-	// t.printTree(&t)
+	t.printTree(t)
 	fmt.Println()
-	// fmt.Println("t: ", t)
-	// fmt.Println(reflect.TypeOf(t))
-	// fmt.Println("&t: ", &t)
-	// // fmt.Println(reflect.TypeOf(&t))
-	// fmt.Println("t: ", t.root)
-	// fmt.Println(reflect.TypeOf(t.root))
-	// fmt.Println("&t: ", &t.root)
-
-	// fmt.Println(reflect.TypeOf(&t.root))
-	// fmt.Println("*t: ", *t.root)
-	// fmt.Println(reflect.TypeOf(*t.root))
-	// fmt.Println("&*t: ", &*t.root)
-	// fmt.Println(reflect.TypeOf(&*t.root))
-	// fmt.Println()
-	// p := &t.root
-	// *p = nil
-	// t.remove(&t, 1)
-
-	// fmt.Println("root : ", t.root)
-	// fmt.Println(reflect.TypeOf(t.root))
-	// fmt.Println("root : ", &t.root)
-	// fmt.Println(reflect.TypeOf(&t.root))
-	// fmt.Println("root : ", *t.root)
-	// fmt.Println(reflect.TypeOf(*t.root))
-	// fmt.Println(reflect.TypeOf(*&t.root))
-	// // *&t.root = nil
-	// fmt.Println("root : ", t.root)
-
-	// fmt.Println()
-
-	// test := &t.root
-	// fmt.Println("test: ", test)
-	// fmt.Println("test: ", &test)
-	// fmt.Println("test: ", *test)
-	// *test = nil
-	// fmt.Println("root : ", t.root)
-
-	// test1 := test
-	// fmt.Println("test1: ", test1)
-	// // *test1 = nil
-	// fmt.Println("test1: ", test1)
-	// // t.root = nil
-	// fmt.Println("test change: ", test)
-	// // test.value = 23
-	// // test = &Node{}
-	// // test.left = nil
-	// fmt.Println("test changeed: ", test)
-	// fmt.Println("root : ", t)
-
-	// t.printTree(t)
-
-	// n := t.root.searchIndex(t.root, 1)
-	// fmt.Println("index  1: ", n)
-
-	// n = t.root.searchIndex(t.root, 1)
-	// fmt.Println("index  1: ", n)
-
-	t.printTree(&t)
-
-	// n = n.getElementSideLeft(t.root)
-	// fmt.Println("node end of side left: ", n)
-
-	// if n.parent.left == n {
-	// 	fmt.Println("compare left true")
-	// }
-	// if n.parent.right == n {
-	// 	fmt.Println("compare right true")
-	// }
-
-	// check := t.searchValue(t, 4)
-	// fmt.Println("check search", check)
-
-	// t.remove(t, 1)
-	// check = t.searchValue(t, 4)
-	// fmt.Println("check search", check)
 
 }
--- part 1/test/testPointer.go
+++ part 1/test/testPointer.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"strconv"
 	"unsafe"
 )
 
 func main() {
+	a := 2
+	p := &a
+	*p = 3
+	fmt.Println("a: ", a)
+
+	// struct
 	type Book struct {
 		Pages int
 		title string
@@ -15,34 +19,14 @@ func main() {
 	var book = Book{} // book is addressable
 	fmt.Println("book: ", book)
 	fmt.Println("book: ", &book)
-	s1 := fmt.Sprintf("%p", &book)
-	fmt.Println(reflect.TypeOf(s1))
-	i, err := strconv.ParseInt(s1, 16, 64)
-	if err != nil {
-		fmt.Println("i convert: ", i)
-	} else {
-		fmt.Println("err: ", err)
-	}
-	fmt.Println("sprintf", s1)
-	fmt.Println("book title: ", book.title)
-	fmt.Println("& book title: ", &book.title)
-	b := &book
-	p := &book.Pages
-	s := &book.title
+	p2 := &book
+	*p2 = Book{1, "hwk"}
+	// *p2 = Book{}
+	fmt.Println("p2:", p2)
+	fmt.Println("book", book)
 	pointer := unsafe.Pointer(&book)
-	fmt.Println("&book: ", pointer)
-	fmt.Println("&b: ", &b)
-	fmt.Println("&p: ", &p)
-	fmt.Println("s: ", s)
-	fmt.Println("&s: ", &s)
-	fmt.Println("&s: ", *&s)
-	*p = 123
-	fmt.Println(book) // {123}
+	fmt.Println("pointer: ", pointer)
+	// invalid indirect of pointer
+	// *pointer = Book{4, "change value"}
 
-	// The following two lines fail to compile,
-	// for Book{}.Pages is not addressable.
-	/*
-		Book{}.Pages = 123
-		p = &(Book{}.Pages) // <=> p = &(Book{}.Pages)
-	*/
 }
