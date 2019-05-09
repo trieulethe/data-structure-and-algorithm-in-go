02a4183268db247e5410d9ad0000bea57ae506a2
trieult <trieult@kaopiz.com>
2019-05-09 08:28:48 +0700

    improvement performance composeRange dynamic programming

--- part 2/dynamic programing/composeRange.js
+++ part 2/dynamic programing/composeRange.js
@@ -3,33 +3,69 @@
 // composeRanges(nums) = ["-1->2", "6->7", "9"]
 
 function composeRanges(nums) {
-  let result = []
+//   let result = []
+
+//   const length = nums.length
+//   if (length === 0) return []
+//   if (length === 1) return [nums[0].toString()]
+//   let headNumber = nums[0]
+//   let tailNumber = nums[0]
+//   result.push(tailNumber.toString())
+
+//   for (let i=1; i<length; i++) {
+//     if (tailNumber + 1 === nums[i]) {
+//       tailNumber = nums[i]
+      
+
+//       // if (result[result.length - 1][0] === '-') {
+//         // result[result.length - 1] = `${result[result.length - 1][0]}${result[result.length - 1][1]}->${nums[i]}`
+//       // } else {
+//         // result[result.length - 1] = `${result[result.length - 1][0]}->${nums[i].toString()}`
+//       // }
+      
+//     } else {
+//       if (headNumber !== tailNumber) {
+//         result.push(nums[i].toString())
+//       }
+//       tailNumber = nums[i]
+//     }
+//   }
+
+//   return result
+
+  // improvement performance
+  const result = []
 
   const length = nums.length
   if (length === 0) return []
   if (length === 1) return [nums[0].toString()]
+
   let headNumber = nums[0]
   let tailNumber = nums[0]
-  result.push(tailNumber.toString())
 
-  for (let i=1; i<length; i++) {
+  for (let i = 1; i < length; i++) {
     if (tailNumber + 1 === nums[i]) {
-
-
-      // if (result[result.length - 1][0] === '-') {
-        // result[result.length - 1] = `${result[result.length - 1][0]}${result[result.length - 1][1]}->${nums[i]}`
-      // } else {
-        // result[result.length - 1] = `${result[result.length - 1][0]}->${nums[i].toString()}`
-      // }
-      
       tailNumber = nums[i]
     } else {
-      result.push(nums[i].toString())
+      if (headNumber !== tailNumber) {
+        result.push(`${headNumber}->${tailNumber}`)
+      } else {
+        result.push(`${headNumber}`)
+      }
+      headNumber = nums[i]
       tailNumber = nums[i]
     }
   }
 
+  if (headNumber !== tailNumber) {
+    result.push(`${headNumber}->${tailNumber}`)
+  } else {
+    result.push(`${headNumber}`)
+  }
+
   return result
 } 
+
+
 
 console.log(composeRanges([-1, 0, 1, 2, -6, 7, 9]))
